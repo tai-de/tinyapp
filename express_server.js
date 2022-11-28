c4fc6579d00eb888773e9ad2ddfc1fe353578faa
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-11-28 10:33:32 -0700

    add page for new url shortener and function for generating random string

--- express_server.js
+++ express_server.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = function() {
+  const existingKeys = Object.keys(urlDatabase);
+  let newKey = Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+  if (existingKeys.includes(newKey)) {
+    newKey = generateRandomString();
+  }
+  return newKey;
+};
+
+console.log(generateRandomString());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,9 +31,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 // app.get("/urls.json", (req, res) => {
